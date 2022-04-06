2bd2b5ff117a1bd5bb3e113ca67e3d90e8cc4a0c
tinaserra <vserra@student.42.fr>
2022-04-06 17:55:10 +0200

    [add] make pair

--- make_pair.hpp
+++ make_pair.hpp
@@ -6,11 +6,22 @@
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 18:23:39 by vserra            #+#    #+#             */
-/*   Updated: 2022/03/25 18:23:52 by vserra           ###   ########.fr       */
+/*   Updated: 2022/04/06 17:45:12 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef MAKE_PAIR_HPP
 # define MAKE_PAIR_HPP
+
+namespace ft {
+
+	template <class T1, class T2>
+	pair<T1,T2> make_pair (T1 x, T2 y)
+	{
+		ft::pair<T1, T2>	retPair(x, y);
+
+		return (retPair);
+	}
+};
 
 #endif
