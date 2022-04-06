5ca18ea8ab6a26888c7f84f8c4cbee33ed479390
tinaserra <vserra@student.42.fr>
2022-04-06 17:54:44 +0200

    [add] lexico machin truc

--- lexicographical_compare.hpp
+++ lexicographical_compare.hpp
@@ -6,11 +6,31 @@
 /*   By: vserra <vserra@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/03/25 18:23:01 by vserra            #+#    #+#             */
-/*   Updated: 2022/03/25 18:23:32 by vserra           ###   ########.fr       */
+/*   Updated: 2022/04/06 17:44:19 by vserra           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef LEXICOGRAPHICAL_COMPARE_HPP
 # define LEXICOGRAPHICAL_COMPARE_HPP
+
+namespace ft {
+
+	template <class InputIterator1, class InputIterator2>
+	bool lexicographical_compare(InputIterator1 first1, InputIterator1 last1,
+			InputIterator2 first2, InputIterator2 last2)
+	{
+		while (first1 != last1)
+		{
+			if (first2 == last2 || *(first2) < *(first1))
+				return (false);
+			else if (*(first1) < *(first2))
+				return (true);
+			++first1;
+			++first2;
+		}
+		return (first2 != last2);
+	}
+
+};
 
 #endif
