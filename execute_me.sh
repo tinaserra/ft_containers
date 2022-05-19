720ede810d073e7eb9d2e638b27cc688f4955a5a
jode-vri <jode-vri@student.42.fr>
2022-05-19 13:15:47 +0000

    [update] run script

--- execute_me.sh
+++ execute_me.sh
@@ -2,3 +2,9 @@ make stl
 ./ft_containers vector
 ./ft_containers stack
 ./ft_containers map
+make
+./ft_containers vector
+./ft_containers stack
+./ft_containers map
+
+# faire vim diff et afficher un message
